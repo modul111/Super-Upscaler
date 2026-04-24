cbf855920b92e4e8e499eb397dafbc79605672f6
modul111 <124570513+modul111@users.noreply.github.com>
2026-04-24 12:14:03 +0300

    Add files via upload

--- server.ts
+++ server.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { createServer as createViteServer } from "vite";
+// import { createServer as createViteServer } from "vite";
 import path from "path";
 import cors from "cors";
 import sharp from "sharp";
